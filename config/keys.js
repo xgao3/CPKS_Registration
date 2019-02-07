183580d3e676d961dcd45ce90bf2eb1234dc705c
xgao3 <xgao.sales@gmail.com>
2019-02-07 10:27:14 -0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-
-
-module.exports = {
-    google: {
-         clientID: '674559829442-9iaasbak6293lbqp1qllf9r1re94ljn5.apps.googleusercontent.com',
-         clientSecret: 'GPOonenlwxXoJNLFjQFbVpzR'
-    }
-}
